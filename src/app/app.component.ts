a35c06ae0d84b35541ea93303a84597c549dba81
Sampson Orson Jackson <sampson.orson@gmail.com>
2023-05-24 21:40:07 +0100

    fix: merge conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Functions, httpsCallable } from '@angular/fire/functions';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  private functions: Functions = inject(Functions);
+  private generateImage = httpsCallable(this.functions, 'generateImage');
 
+  generateNewImage(query: string): void {
+    this.generateImage({ query }).then(result => {
+      console.log(result);
+    })
+  }
 }
