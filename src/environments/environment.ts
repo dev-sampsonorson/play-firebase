7683967b9029cdacd7b5c3b07826fc7fb68b486c
Sampson Orson Jackson <sampson.orson@gmail.com>
2023-05-24 20:58:19 +0100

    chore: setup firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'play-firebase-8447a',
+    appId: '1:629162988064:web:36aaf3784b6fe28ab1e245',
+    storageBucket: 'play-firebase-8447a.appspot.com',
+    apiKey: 'AIzaSyACwo28fCNEA1Uda8BjKMmJ630zBhEjFIY',
+    authDomain: 'play-firebase-8447a.firebaseapp.com',
+    messagingSenderId: '629162988064',
+  },};
